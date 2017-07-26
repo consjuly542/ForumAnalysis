135cabbdb14db358d46dc0ad5e67c8aeb8965eb7
ErmolaevPA <Ermolaev.p.a@yandex.ru>
2017-07-26 14:25:21 +0300

    fixed

--- source/PlotsMaker.py
+++ source/PlotsMaker.py
@@ -21,7 +21,7 @@ def plot_dates(data, path):
                    labelbottom="off", left="off", right="off", labelleft="off") 
     ax.set_ylabel('всего упоминаний')
     ax.set_xlabel('временной интервал')
-    ax.yaxis.set_ticks_position('none') 
+    ax.yaxis.set_ticks_position('none')  
 
     ax.get_xaxis().tick_bottom()    
     ax.get_yaxis().tick_left()  
@@ -32,11 +32,11 @@ def plot_dates(data, path):
     ax.spines["left"].set_visible(False)   
 
     ax.yaxis.set_major_locator(MaxNLocator(integer=True))
-       
-    if len(Counter(data)) > 5:
+	if len(Counter(data)) > 2:
+		data = [d for d in data if d.year>2015]
         ax.hist(data, alpha=0.75, color="#0072B2", linewidth=0.5, bins=12, edgecolor='white')  
     else:
-        c = dict(Counter(data))  
+		c = dict(Counter(data))  
         l = sorted(c.keys())  
         x, y = zip(*sorted(Counter(data).items(), key = operator.itemgetter(0),reverse = True))
 
@@ -46,13 +46,14 @@ def plot_dates(data, path):
         maxy = max(y)
         plt.ylim(0, maxy+1)
         plt.bar(x, y, linewidth=1, color="#0072B2",edgecolor="#0072B2", alpha=0.75)
-
-    myFmt = mdates.DateFormatter('%d/%m')
+    
+	myFmt = mdates.DateFormatter('%d/%m/%Y')
     ax.xaxis.set_major_formatter(myFmt)
     plt.gcf().autofmt_xdate()
     plt.savefig(path+".png")
     plt.close('all')
     
-def test(path):
-    data = [date(2017, random.randint(1, 12), random.randint(1, 28)) for i in range(2000)]  
+def test(path, data=None):
+    if data == None:
+        data = [date(2017, random.randint(1, 12), random.randint(1, 28)) for i in range(2000)]  
     plot_dates(data, path)
